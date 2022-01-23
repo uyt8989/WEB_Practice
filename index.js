dcf084ccf0dc1991a502e98d30072a5906d40b74
kyungyun <kyungyun98@naver.com>
2022-01-23 15:50:39 +0900

    #7

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({exteded: true}));
+//application/json
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://kyungyun:abcd1234@cluster0.iwrz5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
     //useNewUrlParser:true,  useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false
@@ -9,5 +17,19 @@ mongoose.connect('mongodb+srv://kyungyun:abcd1234@cluster0.iwrz5.mongodb.net/myF
 .catch(err => console.log(err))
 
 app.get('/', (req, res) => res.send('Hello World!!!!!'))
+
+app.post('/register', (req, res) => {
+    //회원 가입 할때 필요한 정보들을 client에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) =>{
+        if(err) return res.json({ success : false, err})
+        return res.status(200).json({
+            success : true
+        })
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening an port ${port}!`))
