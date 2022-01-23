84fb5d0f7c7b7699aebb26af8728d19f8ed7b26a
kyungyun <kyungyun98@naver.com>
2022-01-23 16:11:21 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const app = express()
 const port = 5000
 
 const bodyParser = require('body-parser');
+
+const config = require('./config/key');
+
 const { User } = require("./models/User");
 
 //application/x-www-form-urlencoded
@@ -11,7 +14,7 @@ app.use(bodyParser.urlencoded({exteded: true}));
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://kyungyun:abcd1234@cluster0.iwrz5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     //useNewUrlParser:true,  useUnifiedTopology: true, useCreateIndex: true, useFindAndModify:false
 }).then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
