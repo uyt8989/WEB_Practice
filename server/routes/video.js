ed377219c83bd957d8a234c05cd8ac7f53cd6456
movie <kyungyun98@naver.com>
2022-02-15 22:30:14 +0900

    Video Detail

--- server/routes/video.js
+++ server/routes/video.js
@@ -58,5 +58,15 @@ router.get('/getVideos', (req, res) => {
         })
 })
 
+router.post('/getVideoDetail', (req, res) => {
+    // 비디오들을 DB에서 가져와서 클라이언트에 보낸다.
+   Video.findOne({ "_id" : req.body.videoId })
+   .populate('writer')
+   .exec((err, videoDetail) => {
+       if(err) return res.status(400).send(err)
+       return res.status(200).json({ success: true, videoDetail })
+   })
+})
+
 
 module.exports = router;
